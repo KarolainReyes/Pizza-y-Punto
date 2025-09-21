5538fbdc2064763d62825f67d969794b29ef057e
Andre07G <eleal087@gmail.com>
2025-09-21 02:47:42 -0500

    feat: :computer: Add main CLI menu with submenus by@Andre07g

---
+++ cli/menuPrincipal.js
@@ -0,0 +1,222 @@
+import inquirer from "inquirer";
+
+// Importar comandos
+import AgregarPizzaCommand from "../commands/agregarPizza.js";
+import RegistrarPedido from "../commands/reqistrarPedido.js";
+
+import PizzasService from "../services/PizzasService.js";
+import IngredientesService from "../services/IngredientesService.js";
+import RepartidoresService from "../services/RepartidoresService.js";
+import PedidosService from "../services/PedidosService.js";
+import ClientesService from "../services/ClientesService.js";
+
+export default async function menuPrincipal(base, cliente) {
+    // Instanciar servicios
+    const pizzaService = new PizzasService(base, cliente);
+    const ingredientesService = new IngredientesService(base, cliente);
+    const repartidorService = new RepartidoresService(base, cliente);
+    const pedidoService = new PedidosService(base, cliente);
+    const clienteService = new ClientesService(base, cliente);
+
+    let salir = false;
+    console.log("Bienvenido");
+
+    while (!salir) {
+        const { opcion } = await inquirer.prompt([
+            {
+                type: "list",
+                name: "opcion",
+                message: "Selecciona una opción:",
+                choices: [
+                    { name: " Gestionar Pizzas ", value: "1" },
+                    { name: " Gestionar Pedidos ", value: "2" },
+                    { name: " Gestionar Clientes ", value: "3" },
+                    { name: " Gestionar Repartidores ", value: "4" },
+                    { name: " Gestionar Almacen ", value: "5" },
+                    { name: " Salir ", value: "6" }
+                ]
+            }
+        ]);
+
+        switch (opcion) {
+            case "1":
+                await subMenuPizzas(pizzaService, ingredientesService);
+                break;
+            case "2":
+                await subMenuPedidos(pedidoService, pizzaService, clienteService, repartidorService);
+                break;
+            case "3":
+                await subMenuClientes(clienteService);
+                break;
+            case "4":
+                await subMenuRepartidores(repartidorService);
+                break;
+            case "5":
+                await subMenuAlmacen(ingredientesService);
+                break;
+            case "6":
+                salir = true;
+                console.log("Saliendo...");
+                break;
+        }
+    }
+}
+
+/* ------------------- SUBMENUS ------------------- */
+
+async function subMenuPizzas(pizzaService, ingredientesService) {
+    const { opcionPizza } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "opcionPizza",
+            message: "Selecciona una opción:",
+            choices: [
+                { name: " Agregar Pizza ", value: "1" },
+                { name: " Editar Pizzas ", value: "2" },
+                { name: " Eliminar Pizza ", value: "3" },
+                { name: " Ver Pizzas ", value: "4" },
+                { name: " Volver ", value: "5" }
+            ]
+        }
+    ]);
+
+    switch (opcionPizza) {
+        case "1":
+            await new AgregarPizzaCommand({ pizzaService, ingredientesService }).execute();
+            break;
+        case "2":
+            console.log("Editar Pizza - por implementar");
+            break;
+        case "3":
+            console.log("Eliminar Pizza - por implementar");
+            break;
+        case "4":
+            console.log("Eliminar Pizza - por implementar");
+            break;
+        case "5":
+            return;
+    }
+}
+
+async function subMenuPedidos(pedidoService, pizzaService, clienteService, repartidorService) {
+    const { opcionPedido } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "opcionPedido",
+            message: "Selecciona una opción:",
+            choices: [
+                { name: " Agregar Pedido ", value: "1" },
+                { name: " Eliminar Pedido ", value: "2" },
+                { name: " Ver Pedidos ", value: "3" },
+                { name: " Volver ", value: "4" }
+            ]
+        }
+    ]);
+
+    switch (opcionPedido) {
+        case "1":
+            console.log("Eliminar Pizza - por implementar");
+            break;
+        case "2":
+            console.log("Eliminar Pedido - por implementar");
+            break;
+        case "3":
+            console.log("Eliminar Pizza - por implementar");
+            break;
+        case "4":
+            return;
+    }
+}
+
+async function subMenuClientes(clienteService) {
+    const { opcionCliente } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "opcionCliente",
+            message: "Selecciona una opción:",
+            choices: [
+                { name: " Agregar Cliente ", value: "1" },
+                { name: " Eliminar Cliente ", value: "2" },
+                { name: " Ver Clientes ", value: "3" },
+                { name: " Volver ", value: "4" }
+            ]
+        }
+    ]);
+
+    switch (opcionCliente) {
+        case "1":
+            console.log("Eliminar Pizza - por implementar");
+            break;
+        case "2":
+            console.log("Eliminar Cliente - por implementar");
+            break;
+        case "3":
+            console.log("Eliminar Pizza - por implementar");
+            break;
+        case "4":
+            return;
+    }
+}
+
+async function subMenuRepartidores(repartidorService) {
+    const { opcionRepartidor } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "opcionRepartidor",
+            message: "Selecciona una opción:",
+            choices: [
+                { name: " Agregar Repartidor ", value: "1" },
+                { name: " Eliminar Repartidor ", value: "2" },
+                { name: " Ver Repartidores ", value: "3" },
+                { name: " Volver ", value: "4" }
+            ]
+        }
+    ]);
+
+    switch (opcionRepartidor) {
+        case "1":
+            console.log("Eliminar Pizza - por implementar");
+            break;
+        case "2":
+            console.log("Eliminar Repartidor - por implementar");
+            break;
+        case "3":
+           console.log("Eliminar Pizza - por implementar");
+            break;
+        case "4":
+            return;
+    }
+}
+
+async function subMenuAlmacen(ingredientesService) {
+    const { opcionAlmacen } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "opcionAlmacen",
+            message: "Selecciona una opción:",
+            choices: [
+                { name: " Agregar Ingrediente ", value: "1" },
+                { name: " Eliminar Ingrediente ", value: "2" },
+                { name: " Editar Ingrediente ", value: "3" },
+                { name: " Ver Ingredientes ", value: "4" },
+                { name: " Volver ", value: "5" }
+            ]
+        }
+    ]);
+
+    switch (opcionAlmacen) {
+        case "1":
+            console.log("Eliminar Pizza - por implementar");
+        case "2":
+            console.log("Eliminar Ingrediente - por implementar");
+            break;
+        case "3":
+            console.log("Editar Ingrediente - por implementar");
+            break;
+        case "4":
+            console.log("Eliminar Pizza - por implementar");
+            break;
+        case "5":
+            return;
+    }
+}
