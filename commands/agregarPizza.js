3c638cb8bc6505a03bf0d9462e91d7f734f585a4
Karolain Reyes <reyeskarol0412@gmail.com>
2025-09-20 23:18:43 -0500

    feat: :sparkles: Add agregarPizza as Command
    by@KarolainReyes

---
+++ commands/agregarPizza.js
@@ -0,0 +1,33 @@
+
+import inquirer from "inquirer";
+import PizzaService from "../application/PizzaService.js";
+
+export default class AgregarPizzaCommand {
+  constructor({ pizzaService = new PizzaService() } = {}) {
+    this.pizzaService = pizzaService;
+  }
+
+  async execute() {
+    console.clear();
+    console.log("=== ➕ Agregar Pizza ===");
+
+    const answers = await inquirer.prompt([
+      { type: "input", name: "nombre", message: "Nombre de la pizza:" },
+      { type: "input", name: "categoria", message: "Categoría:" },
+      { type: "number", name: "precio", message: "Precio:" },
+      { type: "input", name: "ingredientes", message: "IDs de ingredientes (coma separada):" }
+    ]);
+
+    try {
+      await this.pizzaService.agregarPizza({
+        nombre: answers.nombre,
+        categoria: answers.categoria,
+        precio: answers.precio,
+        ingredientes: answers.ingredientes.split(",")
+      });
+      console.log("✅ Pizza agregada con éxito");
+    } catch (err) {
+      console.error("❌ Error al agregar pizza:", err.message);
+    }
+  }
+}
